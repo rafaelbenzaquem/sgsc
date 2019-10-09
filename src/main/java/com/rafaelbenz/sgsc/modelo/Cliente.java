3aa54fb9b1df2d5a77db87aebcb7258aa2722bf5
Rafael Benzaquem Neto <rafaelbenzaquem@ulbra.edu.br>
2019-10-09 17:05:41 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/rafaelbenz/sgsc/modelo/Cliente.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rafaelbenz.sgsc.modelo;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author adm_rafaelneto
+ */
+public class Cliente {
+    
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nome=" + nome + ", email=" + email + '}';
+    }
+    
+}
---
+++ src/main/java/com/rafaelbenz/sgsc/ui/desktop/ClienteTableModel.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rafaelbenz.sgsc.ui.desktop;
+
+import com.rafaelbenz.sgsc.modelo.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Rafael Benzaquem Neto
+ */
+public class ClienteTableModel extends AbstractTableModel {
+
+    private final List<Cliente> clientes;
+
+    public ClienteTableModel() {
+        clientes = new ArrayList<>();
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    /**
+     *
+     * @param cliente
+     * @return
+     */
+    public boolean addRow(Cliente cliente) {
+        if (clientes.contains(cliente)) {
+            return false;
+        }
+        boolean isAdded = clientes.add(cliente);
+        fireTableDataChanged();
+        return isAdded;
+    }
+
+    /**
+     *
+     * @param id
+     * @param nome
+     * @param email
+     * @return
+     */
+    public boolean addRow(Integer id, String nome, String email) {
+        return addRow(new Cliente(id, nome, email));
+    }
+
+    /**
+     *
+     * @param rowData
+     * @return
+     */
+    public boolean addRow(Object[] rowData) {
+        return addRow(new Cliente((Integer) rowData[0], (String) rowData[1], (String) rowData[2]));
+    }
+
+    public boolean removeAll() {
+        boolean isRemoved = clientes.removeAll(clientes);
+        fireTableDataChanged();
+        return isRemoved;
+    }
+
+    /**
+     *
+     * @param cliente
+     * @return
+     */
+    public boolean removeRow(Cliente cliente) {
+        boolean isRemoved = clientes.remove(cliente);
+        fireTableDataChanged();
+        return isRemoved;
+    }
+
+    /**
+     *
+     * @param id
+     * @param nome
+     * @param email
+     * @return
+     */
+    public boolean removeRow(Integer id, String nome, String email) {
+        return removeRow(new Cliente(id, nome, email));
+    }
+
+    /**
+     *
+     * @param rowData
+     * @return
+     */
+    public boolean removeRow(Object[] rowData) {
+        return removeRow(new Cliente((Integer) rowData[0], (String) rowData[1], (String) rowData[2]));
+    }
+
+    /**
+     *
+     * @param rowIndex
+     * @return
+     */
+    public Object[] getRow(int rowIndex) {
+        if (clientes.isEmpty()) {
+            return null;
+        } else {
+            Cliente cliente = clientes.get(rowIndex);
+            Object[] objects = {cliente.getId(), cliente.getNome(), cliente.getEmail()};
+            return objects;
+        }
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        if (!clientes.isEmpty()) {
+            Cliente cliente = clientes.get(rowIndex);
+
+            switch (columnIndex) {
+                case 0:
+                    return cliente.getId();
+                case 1:
+                    return cliente.getNome();
+                case 2:
+                    return cliente.getEmail();
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0:
+                return "ID";
+            case 1:
+                return "Nome";
+            case 2:
+                return "Email";
+        }
+        return "";
+    }
+
+    @Override
+    public int getRowCount() {
+        return clientes.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+}
